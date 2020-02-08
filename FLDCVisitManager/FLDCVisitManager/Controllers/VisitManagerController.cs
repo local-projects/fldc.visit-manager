8e5bc9e86398482782842ae582d521f478afed78
Tamar <tamarzanzouri@localprojects.com>
2020-02-08 09:18:36 -0500

    first commit - web api for FLDC visit manager

---
+++ FLDCVisitManager/FLDCVisitManager/Controllers/VisitManagerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FLDCVisitManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VisitManagerController : ControllerBase
+    {
+
+        private readonly ILogger<VisitManagerController> _logger;
+
+        public VisitManagerController(ILogger<VisitManagerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void GetCollectionPoints()
+        {
+
+        }
+    }
+}
---
+++ FLDCVisitManager/SquidexDataLayar/SquidexHalper.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SquidexDataLayer
+{
+    public class SquidexHalper
+    {
+    }
+}
