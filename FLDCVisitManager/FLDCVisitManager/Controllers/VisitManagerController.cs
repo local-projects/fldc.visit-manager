c892ff3bfd1447e37aea03f2a264b2ddc52f3bda
Tamar <tamarzanzouri@localprojects.com>
2020-03-12 17:35:04 -0400

    posting LED colors to the cp

--- FLDCVisitManager/FLDCVisitManager/Controllers/VisitManagerController.cs
+++ FLDCVisitManager/FLDCVisitManager/Controllers/VisitManagerController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using FLDCVisitManagerBackend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace FLDCVisitManager.Controllers
 {
@@ -14,6 +18,7 @@ namespace FLDCVisitManager.Controllers
     {
 
         private readonly ILogger<VisitManagerController> _logger;
+        private static bool worked = true;
 
         public VisitManagerController(ILogger<VisitManagerController> logger)
         {
@@ -29,6 +34,35 @@ namespace FLDCVisitManager.Controllers
         [Route("hello")]
         public void GetCollectionPointTrigger(CPRequestParams req)
         {
+            if(worked)
+            SetLEDColors();
+        }
+
+        public async void SetLEDColors()
+        {
+            if (worked)
+            {
+                /*                byte[] byteArr = { 0xFF0000, 0x00FF00, 0x0000FF, 0x00 };*/
+                int[] test = { 0xFF0000, 0x00FF00, 0x0000FF, 0xEE82EE, 0x64 };
+
+                worked = false;
+                var cpIp = "http://192.168.1.185:8080/setLedColorSequence";
+                using var client = new HttpClient();
+                var serializerSettings = new JsonSerializerSettings();
+                serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                var json = JsonConvert.SerializeObject(new LEDRequestParams()
+                {
+                    Pattern = test,
+                    Timer = new List<int>() { 500, 500, 500, 500 },
+                    Cycles = 3,
+                    Run = true
+                }, serializerSettings);
+                //var json = JsonConvert.SerializeObject();
+
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var result = await client.PostAsync(cpIp, data);
+            }
 
         }
     }
---
+++ FLDCVisitManager/FLDCVisitManager/Models/LEDRequestParams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FLDCVisitManagerBackend.Models
+{
+    public class LEDRequestParams
+    {
+        public int[] Pattern { get; set; }
+
+        public List<int> Timer { get; set; }
+
+        public long Cycles { get; set; }
+
+        public bool Run { get; set; }
+        public string SeqName { get; set; }
+    }
+}
