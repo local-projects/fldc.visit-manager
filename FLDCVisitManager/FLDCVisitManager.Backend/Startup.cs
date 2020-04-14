24f3d9aa5a1519ce7bb64d5985dc36606b29de8a
Tamar <tamarzanzouri@localprojects.com>
2020-04-14 10:33:48 -0400

    changed proj naming

---
+++ FLDCVisitManager/FLDCVisitManager.Backend/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FLDCVisitManagerBackend.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using AutoMapper;
+using CMSDataLayer;
+using DBManager;
+using FLDCVisitManagerBackend.BL;
+
+namespace FLDCVisitManager
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            var dom = new ConfigurationBuilder()
+                    .SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json", optional: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                    .Build(); ;
+            Configuration = dom;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            IConfigurationSection cmsSettings = Configuration.GetSection("CMSOptions");
+            services.Configure<AppOptionsConfiguration>(cmsSettings);
+            services.Configure<DataBaseOptions>(Configuration.GetSection("ConnectionStrings"));
+            services.AddAutoMapper(typeof(Startup));
+            services.AddSingleton<ICMSDataHelper, SquidexHelper>();
+            services.AddScoped<IDBManager, SQLHelper>();
+            services.AddScoped<IBusinessLogic, BusinessLogic>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
