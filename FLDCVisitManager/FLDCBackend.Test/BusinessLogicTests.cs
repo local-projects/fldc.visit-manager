b052d75fa9f10da2070727017c74e7f8257f71ab
Tamar <tamarzanzouri@localprojects.com>
2020-04-20 12:18:23 -0400

    added mapping test and finished get all assets test
    fixed issues with get id list and fixed mapping issues

--- FLDCVisitManager/FLDCBackend.Test/BusinessLogicTests.cs
+++ FLDCVisitManager/FLDCBackend.Test/BusinessLogicTests.cs
@@ -11,6 +11,8 @@ using FLDCVisitManager.CMSDataLayar;
 using Microsoft.Extensions.Options;
 using DBManager;
 using Microsoft.Extensions.Configuration;
+using System;
+using FLDCVisitManagerBackend.Helpers;
 
 namespace FLDCBackend.Test
 {
@@ -18,27 +20,35 @@ namespace FLDCBackend.Test
     {
         private static BusinessLogic bl;
         private static IConfigurationRoot _config;
-
+        private static IMapper mapper;
         public BusinessLogicTests()
         {
 
             _config = new ConfigurationBuilder()
              .AddJsonFile("test-app.configuration.json")
              .Build();
-
-            var mapperMock = new Mock<IMapper>();
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMappingModels());
+            });
+            mapper = mockMapper.CreateMapper();
+            Mapper.Reset();
+            Mapper.Initialize(x =>
+                x.AddProfile<AutoMappingModels>()
+            );
+            mapper.ConfigurationProvider.AssertConfigurationIsValid(); //AutoMapping test
             var cmsMock = new Mock<ICMSDataHelper>();
             var cmsOptions = new AppOptionsConfiguration()
             {
                 AppName = _config["CMSOptions:AppName"].ToString(),
                 Url = _config["CMSOptions:Url"].ToString()
             };
-            cmsMock.Setup(x => x.ConnectToCMS(mapperMock.Object.Map<AppOptions>(cmsOptions)));
+            cmsMock.Setup(x => x.ConnectToCMS(mapper.Map<AppOptions>(cmsOptions)));
             var dbMock = new Mock<IDBManager>();
             var dbOptionsMock = new Mock<DatabaseOptions>();
             dbMock.Setup(x => x.SetDBConfiguration(dbOptionsMock.Object.DefaultConnection));
 
-            bl = new BusinessLogic(mapperMock.Object, cmsMock.Object, cmsOptions, dbMock.Object, dbOptionsMock.Object);
+            bl = new BusinessLogic(mapper, cmsMock.Object, cmsOptions, dbMock.Object, dbOptionsMock.Object);
         }
 
         [Theory]
@@ -55,6 +65,62 @@ namespace FLDCBackend.Test
         public void ConvertAssetsToIdList_CPAssetsListShouldReturnListOfIds()
         {
             CollectionPointAssets value = new CollectionPointAssets();
+            GetCPAssets(value);
+            List<CollectibleItemReference> expected = new List<CollectibleItemReference>();
+            GetExpectedAssetIdList(expected);
+            var result = bl.ConvertAssetsToIdList(value);
+            Assert.True(result.Count == 1);
+            Assert.Equal(expected.FirstOrDefault().AssetId, result.FirstOrDefault().AssetId);
+            Assert.Equal(expected.FirstOrDefault().CollectabileId, result.FirstOrDefault().CollectabileId);
+            Assert.Equal(expected.FirstOrDefault().CollectabileType, result.FirstOrDefault().CollectabileType);
+        }
+
+        [Fact]
+        public void ConvertAssetsToCollectibleItems_CPAssetsListShouldReturnListOfCollectibleItems()
+        {
+            CollectionPointAssets value = new CollectionPointAssets();
+            GetCPAssets(value);
+            List<CollectibleItem> expected = new List<CollectibleItem>();
+            GetExpectedCollectabileItems(expected);
+            var result = bl.ConvertAssetsToCollectibleItems(value);
+            Assert.True(result.Count == 1);
+            Assert.Equal(expected.FirstOrDefault().Id, result.FirstOrDefault().Id);
+            Assert.Equal(expected.FirstOrDefault().Caption, result.FirstOrDefault().Caption);
+            Assert.Equal(expected.FirstOrDefault().Credit, result.FirstOrDefault().Credit);
+            Assert.Equal(expected.FirstOrDefault().IconUrl, result.FirstOrDefault().IconUrl);
+            Assert.Equal(expected.FirstOrDefault().ImageUrl, result.FirstOrDefault().ImageUrl);
+            foreach(var item in result.FirstOrDefault().ValuePairs)
+            {
+                Assert.Equal(expected.FirstOrDefault().ValuePairs[item.Key], item.Value);
+            }
+        }
+
+        private void GetExpectedCollectabileItems(List<CollectibleItem> expected)
+        {
+            var item = new CollectibleItem()
+            {
+                Id = "c8ad4314-fd8b-4200-994c-366bfd87ae12",
+                Credit = "Thank you tamar",
+                Caption = "Tamar test",
+                IconUrl = null,
+                ValuePairs = new Dictionary<string, int>() { { "hope", 1 }, { "Fait", 2 }, { "Unity", 3 } },
+                ImageUrl = "https://cloud.squidex.io/api/assets/fldc-prod/c8ad4314-fd8b-4200-994c-366bfd87ae13"
+            };
+            expected.Add(item);
+        }
+
+        private void GetExpectedAssetIdList(List<CollectibleItemReference> expected)
+        {
+            expected.Add(new CollectibleItemReference()
+            {
+                AssetId = "c8ad4314-fd8b-4200-994c-366bfd87ae13",
+                CollectabileId = "c8ad4314-fd8b-4200-994c-366bfd87ae12",
+                CollectabileType = "Image"
+            });
+        }
+
+        private void GetCPAssets(CollectionPointAssets value)
+        {
             value.ImageAssets = new List<ImageAsset>();
             value.QuoteAssets = new List<QuoteAsset>();
             value.ImageAssets.Add(new ImageAsset()
@@ -75,18 +141,12 @@ namespace FLDCBackend.Test
                 }
 
             });
-            List<CollectibleItemReference> expected = new List<CollectibleItemReference>();
-            expected.Add(new CollectibleItemReference()
-            {
-                AssetId = "c8ad4314-fd8b-4200-994c-366bfd87ae13",
-                CollectabileId = "c8ad4314-fd8b-4200-994c-366bfd87ae12",
-                CollectabileType = "Image"
-            });
-            var result = bl.ConvertAssetsToIdList(value);
-            Assert.True(result.Count == 1);
-            Assert.Equal(result.FirstOrDefault().AssetId, value.ImageAssets.FirstOrDefault().Data.ImageAsset.Iv.FirstOrDefault());
-            Assert.Equal(result.FirstOrDefault().CollectabileId, value.ImageAssets.FirstOrDefault().Id.ToString());
-            /*            Assert.Equal(result.FirstOrDefault().CollectabileType, "Image");*/
+        }
+
+        [Fact]
+        public void SerializeObjectToJson_SimpleObjectShouldReturnObjectString()
+        {
+
         }
     }
 }
--- FLDCVisitManager/FLDCVisitManager.Backend/Helpers/AutoMappingModels.cs
+++ FLDCVisitManager/FLDCVisitManager.Backend/Helpers/AutoMappingModels.cs
@@ -18,14 +18,16 @@ namespace FLDCVisitManagerBackend.Helpers
                                                         .ForMember(dest => dest.Timer, src => src.MapFrom(s => (s.Data.TimerSeq.Iv).Select(t => Convert.ToInt32(t.TimerForEachColor)).ToList()))
                                                         .ForMember(dest => dest.Cycles, src => src.MapFrom(s => s.Data.Cycles.Iv))
                                                         .ForMember(dest => dest.Pattern, src => src.MapFrom(
-                                                            s => s.Data.ColorSeq.Iv.Select(x => Convert.ToInt32(x.Colors.Replace("#", "0x"), 16)).ToList()));
+                                                            s => s.Data.ColorSeq.Iv.Select(x => Convert.ToInt32(x.Colors.Replace("#", "0x"), 16)).ToList()))
+                                                        .ForMember(dest => dest.Run, src => src.Ignore());
             CreateMap<CollectionPoint, CPRequestParams>().ForMember(dest => dest.PointName, src => src.MapFrom(s => s.Data.PointName.Iv))
                 .ForMember(dest => dest.CpIp, src => src.MapFrom(s => s.Data.CpIp.Iv))
                 .ForMember(dest => dest.TriggerAnimation, src => src.MapFrom(s => s.Data.TriggerLedColorsSeq))
                 .ForMember(dest => dest.SleepAnimation, src => src.MapFrom(s => s.Data.SleepLedColorsSeq))
+                .ForMember(dest => dest.ReturnAnimation, src => src.Ignore())
                 /*                .ForMember(dest => dest.PointName, src => src.MapFrom(s => s.Data.PointName.Iv))*/
                 ;
-            CreateMap<CPLampIncomingRequest, CPLampData>().ForMember(dest => dest.CPId, src => src.MapFrom(s => s.Id));
+            CreateMap<CPLampIncomingRequest, CPLampData>().ForMember(dest => dest.CPId, src => src.MapFrom(s => s.Id)).ForMember(src => src.AssetId, src => src.Ignore());
             CreateMap<ChargerDockerLampIncomingRequest, CDLampData>();
             CreateMap<List<Iv>, Dictionary<string, int>>()
                 .ConvertUsing(x => 
