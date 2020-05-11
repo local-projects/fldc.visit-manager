b80bbafdaf5ac32e753b4e3ef2d507dd5a26b3a7
Tamar <tamarzanzouri@localprojects.com>
2020-05-11 10:57:17 -0400

    client hub

--- FLDCVisitManager/FLDCVisitManager.Backend/Hubs/ClientHub.cs
+++ FLDCVisitManager/FLDCVisitManager.Backend/Hubs/ClientHub.cs
@@ -1,4 +1,5 @@
 ﻿using FLDCVisitManagerBackend.Models;
+using MessagePack;
 using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
@@ -19,9 +20,22 @@ namespace FLDCVisitManager.Backend.Hubs
             await Clients.All.SendAsync("setClientMessage", "A connection with ID '" + connectionId + "' has just connected");
         }
 
-        public async Task CPCollectedAsset(BeaconsCPLampIncomingRequest req)
+        public async Task CPCollectedAsset(string beaconID, string lampID, string assetsID)
+            //(DataModel req)
         {
-            await Clients.All.SendAsync("CPCollectedAssetSuccess");
+            /*await Clients.All.SendAsync("CPCollectedAssetRecived", req);
+*/            await Clients.All.SendAsync("CPCollectedAssetSuccess");
         }
+
+/*        [MessagePackObject]
+        public class DataModel
+        {
+            [Key("beaconId")]
+            int BeaconID { get; set; }
+            [Key("lampId")]
+            int LampID { get; set; }
+            [Key("assetsId")]
+            int AssetsID { get; set; }
+        }*/
     }
 }
